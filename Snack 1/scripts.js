2a31ce82f0985a5c46ef76e69367479a0b5340cf
Obez99 <giuliogx@live.it>
2021-10-25 14:44:24 +0200

    Creates Snack 1 files and creates bikes object

---
+++ Snack 1/scripts.js
@@ -0,0 +1,26 @@
+const bikes = [
+  {
+    name: "Mountain Bike",
+    weight: 14,
+  },
+
+  {
+    name: "BMX",
+    weight: 9,
+  },
+
+  {
+    name: "Downhill",
+    weight: 16,
+  },
+
+  {
+    name: "Tandem",
+    weight: 20,
+  },
+
+  {
+    name: "Triciclo",
+    weight: 2,
+  },
+]
